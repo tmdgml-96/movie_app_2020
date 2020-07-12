6f2f0573d0779daf59c3afc7d790b6cd360f3f40
tmdgml_96 <dkanxms12@naver.com>
2020-07-12 16:24:42 +0900

    Updates

---
+++ precache-manifest.dc3a289244fddd2af10468e6d698cd06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932c9d639c5b0cb5c3264945dca4127c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e4d1c1ab4c72959cab3a",
+    "url": "/movie_app_2020/static/css/main.9e2fa6c3.chunk.css"
+  },
+  {
+    "revision": "47e4d9f7478af5aba5b2",
+    "url": "/movie_app_2020/static/js/2.f68187fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.f68187fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d1c1ab4c72959cab3a",
+    "url": "/movie_app_2020/static/js/main.8a06753c.chunk.js"
+  },
+  {
+    "revision": "954ada875b29a20b1359",
+    "url": "/movie_app_2020/static/js/runtime-main.44d121aa.js"
+  }
+]);
